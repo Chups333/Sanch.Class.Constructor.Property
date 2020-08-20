477d91a6539e5b490ad1de50e1acd59dca1f9dd5
Petrov Sasha <sasha1327@mail.ru>
2020-08-20 18:38:01 +0800

    Добавьте файлы проекта.

---
+++ Sanch.Class.Constructor.Property/Person.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace Sanch.Class.Constructor.Property
+{
+    public class Person
+    {
+
+     
+
+        public string SecondName { get; private set; }
+        public string Name { get; private set; }
+
+        public Person(string secondName, string name, int age) // конструктор 
+        {
+            if (age<0)
+            { 
+                throw new ArgumentNullException("Меньше");// быстрое выявление ошибки
+            }
+            SecondName = secondName;
+            Name = name;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //public string Name; - нельзя так делать
+        //private string _name;// - можно
+
+
+
+
+
+        ////////////////////////////
+        // Сокращенные свойства
+        /////////////////////////////////
+
+        //public string SecondName { get; /*protected  только для наследника*/ set; } // тоже самое что нижнее только проще
+
+
+       /* public string FullName //полное имя
+        {
+            get
+            {
+                return SecondName + " " + Name;
+            }
+        }*/
+        /*public string ShortName // короткое имя
+        {
+            get
+            {
+                return $"{SecondName} {Name.Substring(0, 1)}.";
+            }
+        }*/
+
+        ////////////////////////////
+        // Свойства get и set
+        /////////////////////////////////
+       /* public string Name //cвойства
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                /* if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Имя не может быть пустым");// быстрое выявление ошибки
+                }
+                _name = value;
+            }
+        }*/
+
+
+
+
+
+
+        //////////////////////////////////////// 
+        //Ниже конструкция которую ты пишешь сам ручками
+        ////////////////////////////////////////
+        /*public string GetName()// конструкция 1
+        {
+            return _name;
+
+        }
+        public void SetName(string name)// конструкция 2
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя не может быть пустым");// быстрое выявление ошибки
+            }
+            _name = name;
+        }*/
+    }
+}
---
+++ Sanch.Class.Constructor.Property/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sanch.Class.Constructor.Property
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Person p = new Person("Пупкин", "Вася", 11); //удобнее
+
+
+            //p.SetName("Вася"); - выдаст имя Вася 
+            /* p.SetName("");//перейдет на строку с ошибкой
+
+            Console.WriteLine(p.GetName());
+            Console.ReadKey();*/
+
+            /* p.Name = "Вася";
+
+            Console.WriteLine(p.Name);
+            Console.ReadKey();*/
+
+
+        }
+    }
+}
